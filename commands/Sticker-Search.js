057f711306af6275c93460bd6be190b2b6c6a838
🇱🇰MR_KASUN-MD <168183976+Feenixmd0@users.noreply.github.com>
2024-04-30 15:06:14 +0530

    🍀MR_KASUN🍁

--- commands/Sticker-Search.js
+++ commands/Sticker-Search.js
@@ -1,1 +1,68 @@
+/** 
+
+𝑭 𝑬 𝑬 𝑵 𝑰 𝑿-𝑴𝑫
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙eenix 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝑲𝑨𝑺𝑼𝑵 𝑺𝑨𝑵𝑫𝑬𝑬𝑷𝑨 
+ 
+ **/
+
+
+
+
+
+
+const axios = require("axios");
+const { Sticker, StickerTypes } = require("wa-sticker-formatter");
+const {kasun} = require("../framework/kasun");
+
+kasun({
+  nomCom: "stickersearch",
+  categorie: 'Search',
+  reaction: "🍁"
+},
+async (dest, zk, commandeOptions) => {
+  const { repondre, ms, arg, nomAuteurMessage } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("where is the request ? !");
+    return;
+  }
+
+  const gifSearchTerm = arg.join(" ");
+  const tenorApiKey = "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c"; // Remplacez par votre clé d'API Tenor
+
+  try { for ( i = 0 ; i < 5 ; i++) {
+    const gif = await axios.get(
+      `https://tenor.googleapis.com/v2/search?q=${gifSearchTerm}&key=${tenorApiKey}&client_key=my_project&limit=8&media_filter=gif`
+    );
+
+    const gifUrl = gif.data.results[i].media_formats.gif.url;
+
+    
+   
+
+    // Assurez-vous de remplacer les valeurs manquantes dans la création du sticker
+    const packname = nomAuteurMessage; // Remplacez par le nom de votre pack de stickers
+
+    const stickerMess = new Sticker(gifUrl, {
+      pack: packname,
+      author: 'FEENIX-MD',
+      type: StickerTypes.FULL,
+      categories: ["🤩", "🎉"],
+      id: "12345",
+      quality: 60,
+      background: "transparent",
+    });
+    const stickerBuffer2 = await stickerMess.toBuffer();
+    zk.sendMessage(dest, { sticker: stickerBuffer2 }, { quoted: ms }); }
+  } catch (error) {
+    console.error("Erreur lors de la recherche de stickers :", error);
+    repondre("Erreur lors de la recherche de stickers.");
+  }
+});
 
